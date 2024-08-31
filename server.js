398ff50323449d04b594b180f6c0ca0088e00f4b
Facundo García Martoni <thunderxforce@gmail.com>
2024-08-31 15:16:59 -0300

    🩹 Implemented the latest version of OpenAI API

--- server.js
+++ server.js
@@ -1,31 +1,34 @@
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 dotenv.config();
 
-import { Configuration, OpenAIApi } from 'openai';
-import { convertCompilerOptionsFromJson } from 'typescript';
+import OpenAI from "openai";
+import { convertCompilerOptionsFromJson } from "typescript";
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI
+// const configuration = new Configuration({
+//   apiKey: process.env.OPENAI
+// });
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI,
+  organization: "org-cmcDfj8satFOMB1bd9i9pTgN",
 });
 
-const openai = new OpenAIApi(configuration);
-
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post('/dream', async (req, res) => {
+app.post("/dream", async (req, res) => {
   const prompt = req.body.prompt;
-  const aiResponse = await openai.createImage({
+  const aiResponse = await openai.images.generate({
     prompt,
     n: 1,
-    size: '1024x1024'
-  })
+    size: "1024x1024",
+  });
   const image = aiResponse.data.data[0].url;
-  res.send({ image })
+  res.send({ image });
 });
 
-app.listen(8080, () => console.log('make art on http://localhost:8080/dream'));
+app.listen(8080, () => console.log("make art on http://localhost:8080/dream"));
