5e67283e8a0c386e236f509b05498586c397ff77
Facundo García Martoni <thunderxforce@gmail.com>
2024-09-03 12:07:12 -0300

    ⚠️  Added Error Handling

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import "./style.css";
 const form = document.querySelector("form");
 
 form.addEventListener("submit", async (e) => {
-  showSpinner()
+  showSpinner();
   e.preventDefault();
   const data = new FormData(form);
   const response = await fetch("http://localhost:8080/dream", {
@@ -15,11 +15,16 @@ form.addEventListener("submit", async (e) => {
       prompt: data.get("prompt"),
     }),
   });
-  const { image } = await response.json();
-
-  const result = document.querySelector("#result");
-  result.innerHTML = `<img src="${image}" width='512' />`;
-  hideSpinner()
+  if (response.ok) {
+    const { image } = await response.json();
+    const result = document.querySelector("#result");
+    result.innerHTML = `<img src="${image}" width='512' />`;
+  } else {
+    const err = await response.text();
+    alert(err);
+    console.error(err);
+  }
+  hideSpinner();
 });
 
 function showSpinner() {
--- server.js
+++ server.js
@@ -21,14 +21,19 @@ app.use(cors());
 app.use(express.json());
 
 app.post("/dream", async (req, res) => {
-  const prompt = req.body.prompt;
-  const aiResponse = await openai.images.generate({
-    prompt,
-    n: 1,
-    size: "1024x1024",
-  });
-  const image = aiResponse.data[0].url;
-  res.send({ image });
+  try {
+    const prompt = req.body.prompt;
+    const aiResponse = await openai.images.generate({
+      prompt,
+      n: 1,
+      size: "1024x1024",
+    });
+    const image = aiResponse.data[0].url;
+    res.send({ image });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error?.error.message) || "Something went wrong.";
+  }
 });
 
 app.listen(8080, () => console.log("make art on http://localhost:8080/dream"));
